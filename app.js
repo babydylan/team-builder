f03584549be513b8b031cdcfcdc0a579440705f0
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-06 22:24:41 -0700

    added engineer prompt q's

--- app.js
+++ app.js
@@ -47,6 +47,29 @@ function initiatePrompt() {
         choices: ["Engineer", "Intern", "I don't want to add any more team members"]
     },
 
+    {
+        type: "input",
+        name: "engineerName",
+        message: "What is your engineer's name?"
+    },
+
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is your engineer's id?"
+    },
+
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?"
+    },
+
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is your engineer's github?"
+    },
   ])
 }
 
